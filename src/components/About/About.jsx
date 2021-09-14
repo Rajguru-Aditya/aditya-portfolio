0ee8f6f2e3a5010fbbb32a33f12c1439ded8f69b
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-09-14 22:15:04 +0530

    Add about page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./about.css";
+import linkedIn from "../images/linkedin1.png";
+import instagram from "../images/instagram1.png";
+import twitter from "../images/twitter1.png";
+import github from "../images/github-logo.png";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="about-container">
+        <h1 id="about-title">About Me</h1>
+        <p id="about-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <div className="about-contact">
+          <p>Let’s work Together!</p>
+          <p>
+            Ping me an{" "}
+            <a href="/" id="email">
+              email
+            </a>{" "}
+            or DM me on social media. Always up for a chat.
+          </p>
+        </div>
+        <div className="social-icons">
+          <a href="/">
+            <img src={linkedIn} alt="linkedin" />
+          </a>
+          <a href="/">
+            <img src={instagram} alt="instagram" />
+          </a>
+          <a href="/">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="/">
+            <img src={github} alt="github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
